af857ba90237c8673362767788bc633dab011286
jbcguidech-crypto <jbc.guide.ch@gmail.com>
2026-07-14 19:08:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='souffle-v4';
-const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='souffle-v6';
+const ASSETS=['./index.html','./manifest.json','./icon-v2-192.png','./icon-v2-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
